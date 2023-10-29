b481027b648c1fabc1d28341c012e5f08f73c3d3
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-10-29 22:40:46 +0300

    Add objs-arrays-enums.ts

--- app.ts
+++ app.ts
@@ -1,20 +1,15 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]; // tuple type
-} = {
-    name: 'Natali',
-    age: 34,
-    hobbies: ['Sport', 'Cooking'],
-    role: [2, 'author']
-};
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sport'];
-
-console.log(person.name);
-
-for(const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
